df63b801b2de3f5053a22b63fcc17ad2d28c9464
uchitsa <uchitsa@ya.ru>
2022-10-04 01:56:29 +0300

    hw3 starts

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,20 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import "strings"
+
+func Top10(text string) []string {
 	// Place your code here.
-	return nil
+	statMap := make(map[string]int, 0)
+	words := strings.Split(text, " ")
+
+	for _, word := range words {
+		w := strings.Trim(word, "?!,.-")
+		statMap[strings.ToLower(w)]++
+	}
+
+	delete(statMap, "")
+
+	topStats := make([]string, len(statMap))
+
+	return topStats[:10]
 }
