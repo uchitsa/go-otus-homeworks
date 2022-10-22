9b363f1e5c5b26778cf44ec0dbab7a815e836b79
uchitsa <uchitsa@ya.ru>
2022-10-23 02:10:16 +0300

    hw3 wip

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,20 +1,35 @@
 package hw03frequencyanalysis
 
-import "strings"
+import (
+	"math"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var regexpSplit = regexp.MustCompile(`\s-\s|[!"#$%&'()*+,\\./:;<=>?@[\]^_\x60{|}~\s]+`)
 
 func Top10(text string) []string {
 	// Place your code here.
 	statMap := make(map[string]int, 0)
-	words := strings.Split(text, " ")
 
-	for _, word := range words {
-		w := strings.Trim(word, "?!,.-")
+	for _, w := range regexpSplit.Split(text, -1) {
 		statMap[strings.ToLower(w)]++
 	}
 
-	delete(statMap, "")
-
 	topStats := make([]string, len(statMap))
+	for s := range statMap {
+		topStats = append(topStats, s)
+	}
 
-	return topStats[:10]
+	sort.Slice(topStats, func(i, j int) bool {
+		if statMap[topStats[i]] == statMap[topStats[j]] {
+			return topStats[i] < topStats[j]
+		}
+		return statMap[topStats[i]] > statMap[topStats[j]]
+	})
+
+	limit := int(math.Min(float64(len(topStats)), 10))
+
+	return topStats[:limit]
 }
